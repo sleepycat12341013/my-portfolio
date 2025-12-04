e93d76bcd0d83f5b7c56a14079093ba068339d08
sleepycat12341013-boop <sleepycat12341013@gmail.com>
2025-12-04 15:53:01 +0900

    Update site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      animation: {
-        blink: "blink 1s step-start infinite",
-      },
-      keyframes: {
-        blink: {
-          "0%, 50%, 100%": { opacity: 1 },
-          "25%, 75%": { opacity: 0 },
-        },
-      },
-    },
-  },
-  plugins: [],
-};
-
-module.exports = {
-  theme: {
-    extend: {
-      animation: {
-        "bounce-once": "bounce-once 0.3s ease-out 1",
-      },
-      keyframes: {
-        "bounce-once": {
-          "0%": { transform: "translateY(0)" },
-          "50%": { transform: "translateY(-6px)" }, // 上に跳ねる
-          "100%": { transform: "translateY(0)" }, // 元に戻る
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
